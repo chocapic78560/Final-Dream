10edd80b86c8a824b6449566ba17c4fddc862e8d
chocapic78560 <matteo.galus@epita.fr>
2025-05-18 00:09:01 +0200

    teleport marche plus debut de l autre map

--- Assets/Scripts/teleport.cs
+++ Assets/Scripts/teleport.cs
@@ -2,16 +2,16 @@ using UnityEngine;
 
 public class TeleportOnTouch : MonoBehaviour
 {
-    public Vector3 teleportDestination = new Vector3(10f, 0f, 0f);
-
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            other.transform.position = teleportDestination;
-            Debug.Log("TeleportOnTouch");
+            Vector3 newPosition = other.transform.position;
+            newPosition.x += 100f;
+            other.transform.position = newPosition;
         }
     }
 }
+
 
 
